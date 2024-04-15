ee035094c89b16d0d0150436c0bad1aa2e6893eb
mohitbooraget <137650003+mohitbooraget@users.noreply.github.com>
2024-04-15 18:02:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 
-API_ID = API_ID = 25984163
+API_ID = API_ID = 24147139
 
-API_HASH = os.environ.get("API_HASH", "ef8b083047453358ef27bf889e8de82c")
+API_HASH = os.environ.get("API_HASH", "17f525f59de52108805a65f5ffd909e4")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "6841462459:AAF6O_r-rJ3Gixl-4ZlHnzlxL-B_5HLSlpw")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", 6971380203))
+OWNER = int(os.environ.get("OWNER", 5935537622))
 
 LOG = -1002146213576
 
