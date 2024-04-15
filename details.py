89eaa280bce627acd4c5a3647026355024e55149
mohitbooraget <137650003+mohitbooraget@users.noreply.github.com>
2024-04-15 18:10:06 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 22779671
-api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
-bot_token = "6152562853:AAGimPmtvHjqcE8em9iDMH-QAjkM8133P0c"
+api_id = 24147139
+api_hash = "17f525f59de52108805a65f5ffd909e4"
+bot_token = "6636089193:AAHKrRm-qKYpj82MnlQyrJ6AGcFmfwW31TI"
 sudo_groups = -1002049391187
